3accab2bcb8a6cf40867b238387a828bab9cfce8
KernelError <villainkernelerror@gmail.com>
2020-05-21 19:52:54 +0200

    Re-export modules instead of exposing them

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub mod consts;
-pub mod enums;
-pub mod functions;
-pub mod types;
+pub use consts::*;
+pub use enums::*;
+pub use functions::*;
+pub use types::*;
+
+mod consts;
+mod enums;
+mod functions;
+mod types;
