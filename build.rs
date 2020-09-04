d17496dc0f9baa00312d6876abb3cf3846518b3b
KernelError <villainkernelerror@gmail.com>
2020-09-04 21:46:00 +0200

    Fix linker errors on Linux

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("cargo:rustc-link-lib=lib/bass");
+    #[cfg(windows)]
+    {
+        println!("cargo:rustc-link-lib=lib/bass");
+    }
+
+    #[cfg(not(windows))]
+    {
+        println!("cargo:rustc-link-search=./lib");
+        println!("cargo:rustc-link-lib=bass");
+    }
 }
